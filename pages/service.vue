cc735892378d5432e1e6fa1b4301cb98d4fdc6c4
robin <756517553@qq.com>
2018-01-12 14:35:34 +0800

    增加elementUI组件和axios

---
+++ pages/service.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+       this is servcie page
+   </div>
+</template>
+<script>
+export default {
+   name:'service'
+}
+</script>
+<style scoped>
+     div{
+         background-color: red;
+     }
+</style>
+
+
